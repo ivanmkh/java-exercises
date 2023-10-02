b7bafeb81487424ef48274c5020156157eaae43b
Ivan Mkhitaryan <ivan.mkhitaryan@adacta-fintech.com>
2023-10-02 21:20:14 +0300

    init

---
+++ Basics/exercise-1/src/Main.java
@@ -0,0 +1,6 @@
+//  Write a Java program to print 'Hello' on screen and your name on a separate line.
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello" + System.lineSeparator() + "Ivan");
+    }
+}
