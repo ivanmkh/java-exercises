e1a7d29d896cdbb9550c27345ba9c1f9fbb798d3
ivanmkh <mkhitaryanim@gmail.com>
2023-11-07 01:43:08 +0300

    add exercise

--- Basics/exercise-144/src/Main.java
+++ Basics/exercise-144/src/Main.java
@@ -1,3 +1,6 @@
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
 // Write a Java program to remove all occurrences of a specified value in a given array of integers.
 // Return the updated array length.
 // Sample Output:
@@ -5,6 +8,22 @@
 // The length of the new array is: 4
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int[] array = {1, 4, 6, 7, 6, 2};
+        int[] newArray = removeElement(array, 6);
+        System.out.printf("The length of the new array is: %d", newArray.length);
+    }
+
+    public static int[] removeElement(int[] array, int deletingValue) {
+        ArrayList<Integer> list = new ArrayList<>();
+        for (int element : array) {
+            if (element != deletingValue) {
+                list.add(element);
+            }
+        }
+        int[] newArray = new int[list.size()];
+        for (int i = 0; i < list.size(); i++) {
+            newArray[i] = list.get(i);
+        }
+        return newArray;
     }
 }
